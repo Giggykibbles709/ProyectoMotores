41cc8aebe44590ae75a37f7d6262170fc26f4afe
giggykibbles709 <mjrueda@outlook.com>
2025-04-21 21:22:48 +0200

    Camera system

---
+++ Ejercicios/Scripts/CameraManager.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    public Camera camera1; // Jugador 1
+    public Camera camera2; // Jugador 2
+    public Camera isometricCamera; // Cámara isométrica
+    public Camera orthographicCamera; // Cámara ortográfica
+
+    public GameObject ball;
+    public GameObject player1;
+    public GameObject player2;
+
+    private void Start()
+    {
+        ActivateSplitScreen(); // Inicia con la pantalla dividida.
+    }
+
+    private void Update()
+    {
+        // Activar cámara isométrica.
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            ActivateIsometricCamera();
+        }
+
+        // Activar cámara ortográfica.
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            ActivateOrthographicCamera();
+        }
+
+        // Activar pantalla dividida.
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            ActivateSplitScreen();
+        }
+    }
+
+    private void ActivateSplitScreen()
+    {
+        SetCameraState(camera1, true);
+        SetCameraState(camera2, true);
+        SetCameraState(isometricCamera, false);
+        SetCameraState(orthographicCamera, false);
+
+        ball.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+        player1.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation;
+        player2.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation;
+    }
+
+    private void ActivateIsometricCamera()
+    {
+        SetCameraState(camera1, false);
+        SetCameraState(camera2, false);
+        SetCameraState(isometricCamera, true);
+        SetCameraState(orthographicCamera, false);
+
+        ball.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+        player1.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation;
+        player2.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation;
+    }
+
+    private void ActivateOrthographicCamera()
+    {
+        SetCameraState(camera1, false);
+        SetCameraState(camera2, false);
+        SetCameraState(isometricCamera, false);
+        SetCameraState(orthographicCamera, true);
+
+        RigidbodyConstraints constraints = RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezeRotation;
+
+        ball.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePositionX;
+        player1.GetComponent<Rigidbody>().constraints = constraints;
+        player2.GetComponent<Rigidbody>().constraints = constraints;
+    }
+
+    private void SetCameraState(Camera cam, bool state)
+    {
+        if (cam != null)
+        {
+            cam.gameObject.SetActive(state);
+        }
+    }
+}
